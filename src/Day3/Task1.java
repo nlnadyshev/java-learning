6a39454affc44e946b769b74269cd2886ce49bcd
nlandyshev <nlandyshev011@gmail.com>
2025-09-19 14:36:39 +0300

    Day 2 tasks

---
+++ src/Day3/Task1.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int myInt = 777;
+        byte myByte = 20;
+        short myShort = 330;
+        long myLong = 89775179208l;
+        float myFloat = 3.12f;
+        double myDouble = 333.223344;
+        char v = 'A';
+        boolean b = true;
+    }
+}
